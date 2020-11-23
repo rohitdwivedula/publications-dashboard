c4364844af8ed1941a5bcfc0ba8d66dc9cbf7c7a
Rohit Dwivedula <rohitdwivedula@gmail.com>
2020-11-23 19:50:28 +0530

    Add all users page. -> /register -> search for researcher ID implemented. -> login implemented

--- webapp/public_html/utilities/authenticate.php
+++ webapp/public_html/utilities/authenticate.php
@@ -1,25 +1,20 @@
 <?php
-	function authenticate($email, $key){
+	function authenticate($id, $key){
 		require_once("db_connection.php");
-		$query = "SELECT user_key, salt, name FROM users WHERE user_email = '$email';";
+		$query = "SELECT * FROM Researcher WHERE researcher_id = $id;";
 		if($result = getResult($query)){
 			if(mysqli_num_rows($result) == 1){
 				$row = mysqli_fetch_array($result);
-				$salt = $row['salt'];
-				$hashed_key = hash('sha512', $key + $salt);
-				if($row['user_key'] == $hashed_key){
-					//correct password
-					session_start();
-					$_SESSION['name'] = $row['name'];
-					$_SESSION['email'] = $email;
-					header("Location: marks.html");
-					exit();
-				}
-				else{
-					//wrong password
-					header("Location: index.html?error=incorrect");
-					exit();
-				}
+				session_start();
+				$_SESSION['id'] = $id;
+				$_SESSION['name'] = $row['name'];
+				$_SESSION['h5'] = $row["h5_index"];
+				header("Location: researcher.html");
+				exit();
+
+				//wrong password
+				//header("Location: index.html?error=incorrect");
+				//exit();
 			}
 			else{
 				//email not registered
@@ -28,7 +23,7 @@
 			}
 		}
 		else{
-			header("Location: index.html?error=yes");
+			header("Location: index.html?error=yes123");
 			exit();
 		}
 	}
